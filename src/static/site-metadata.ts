e68d35a386c206f1263264c6f6bffcd74351621f
糯金 <zhuyoukang.zyk@alibaba-inc.com>
2025-03-25 15:54:59 +0800

    feat: 更改配置

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,17 +11,17 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: '',
-  logo: 'https://avatars.githubusercontent.com/u/8546109?v=4',
+  siteUrl: 'https://zhuyoukang/running/profile.com',
+  logo: 'https://avatars.githubusercontent.com/u/8546109?v=4', // todo
   description: 'Personal site and blog',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://anchen.me/',
+      url: '', // blog链接
     },
     {
       name: 'About',
-      url: 'https://anchen.me/about/',
+      url: '', // todo
     },
   ],
 };
